3a285e513c4aaded5a8d3990d964ec73e9557ec7
Владимир? <Sobolev.Wladim@yandex.ru>
2023-01-02 11:57:03 +0300

    подклюил openapi

--- src/Controller/InfoController.php
+++ src/Controller/InfoController.php
@@ -3,13 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Annotations as OA;
 
 class InfoController
 {
-    #[Route('/info')]
-    public function files(): Response
+     /**
+     * Получаем список файлов доступных для скачивания на сервере.
+     *
+     * @Route("/api/info", methods={"GET"})
+     * @OA\Response(
+     *     response=200,
+     *     description="Массив с именами файлов",
+     * )
+     */
+    public function files(): JsonResponse
     {
         return new JsonResponse(['hello']);
     }
