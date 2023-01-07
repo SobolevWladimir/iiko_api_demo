47039bb9e01e196bcfda3e9a2080d1adbfeadc61
Владимир? <Sobolev.Wladim@yandex.ru>
2023-01-07 12:43:10 +0300

    сделал авторизацию

---
+++ src/Controller/DeliveryController.php
@@ -0,0 +1,10 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DeliverController extends AbstractController
+{
+    public function getTerminals()
+    {
+    }
+}
